e59fb37006804da5e2e4e964c068518bb13c5422
Lê Trọng Kha <22T1020634@localhost>
2026-08-03 08:33:50 +0700

    Initial ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlogManager_LeTrongKha.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
